e3e329137b63c069e70002e464ad40831b4ba332
bonomoon <moonbonoz@gmail.com>
2020-03-14 12:56:12 +0900

    solve: boj 1002

--- BaekJoon/01002번_터렛/1002.cpp
+++ BaekJoon/01002번_터렛/1002.cpp
@@ -1,48 +1,54 @@
-#include <iostream>
-#include <string>
-#include <cstring>
 #include <cstdio>
+#include <cstdlib>
 #include <cmath>
-typedef struct Turret{
-	int x1, y1, r1;
-	int x2, y2, r2;
-}Turret;
 
-int testRange(int x1, int y1, int r1, int x2, int y2, int r2){
-	double p_dist = sqrt(pow(x2-x1, 2.0) + pow(y2-y1, 2.0)); 
-	int dist = r1 + r2;
+typedef struct _Turret {
+    int x, y, r;
+} Turret;
 
-	if(dist > p_dist){
-		if(x1 == x2 && y1 == y2){
-			if(r1 == r2)
-				return -1;
-			else
-				return 0;
-		}
-		else if(r1-r2 == p_dist || r2-r1 == p_dist)
-			return 1;
-		else if(r1-r2 >= p_dist || r2-r1 >= p_dist)
-			return 0;
-		else
-			return 2;
-	}
-	else if(dist == p_dist)
-		return 1;
-	else
-		return 0;
+double get_distance(int x1, int y1, int x2, int y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
-int main(){
-	int T;
-	scanf("%d", &T);
+int solve(Turret t1, Turret t2) {
+    double d = get_distance(t1.x, t1.y, t2.x, t2.y);
+    int r = t1.r + t2.r;
 
-	Turret t[T];
+    if(d > r) {
+        return 0;
+    } else {
+        if (d == r) {
+            return 1;
+        } else {
+            if(d == 0) {    // 원의 중심이 같을 경우
+                if(t1.r == t2.r) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            } else {
+                if(d == abs(t1.r - t2.r)) {
+                    return 1;
+                } else if(d < abs(t1.r - t2.r)) {
+                    return 0;
+                } else {
+                    return 2;
+                }
+            }
+        }
+    }
+}
 
-	for(int i = 0; i < T; i++){
-		scanf("%d %d %d %d %d %d", &t[i].x1, &t[i].y1, &t[i].r1, &t[i].x2, &t[i].y2, &t[i].r2);
-	}
+int main() {
+    int T = 0;
+    int distance = 0;
+    scanf("%d", &T);
 
-	for(int i = 0; i < T; i++){
-		printf("%d\n", testRange(t[i].x1, t[i].y1, t[i].r1, t[i].x2, t[i].y2, t[i].r2));
-	}
+    for (int i = 0; i < T; ++i) {
+        int d = 0;
+        Turret t1, t2;
+        scanf("%d %d %d %d %d %d", &t1.x, &t1.y, &t1.r, &t2.x, &t2.y, &t2.r);
+        printf("%d\n", solve(t1, t2));
+    }
+    return 0;
 }
