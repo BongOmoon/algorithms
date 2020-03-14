e1e6a701c31a6ba80c9ef74dff88869efd1f21a0
bonomoon <moonbonoz@gmail.com>
2020-03-14 11:51:48 +0900

    solve: boj 1003

--- BaekJoon/01003번_피보나치_함수/1003.cpp
+++ BaekJoon/01003번_피보나치_함수/1003.cpp
@@ -1,40 +1,31 @@
-#include <iostream>
 #include <cstdio>
-#include <vector>
 
-using namespace std;
+void solve(int N) {
+    int zero[41] = {0, }, one[41] = {0, };
 
-void fibonacci(int idx){
-	vector<int> zero;
-	vector<int> one;
+    zero[0] = 1, one[0] = 0;
+    zero[1] = 0, one[1] = 1;
 
-	if(idx == 0){
-		printf("1 0\n");
-	}
-	else if(idx == 1){
-		printf("0 1\n");
-	}
-	else{
-		zero.push_back(1);
-		one.push_back(0);
-		zero.push_back(0);
-		one.push_back(1);
-		for(int i = 2; i <= idx; i++){
-			zero.push_back(zero[i-1] + zero[i-2]);
-			one.push_back(one[i-1] + one[i-2]);
-		}
-		printf("%d %d\n", zero[idx], one[idx]);
-	}
+    for(int i = 2; i <= N; ++i) {
+        if(zero[i] != 0 && one[i] != 0) {
+            continue;
+        } else {
+            zero[i] = zero[i - 1] + zero[i - 2];
+            one[i] = one[i - 1] + one[i - 2];
+        }
+    }
+    printf("%d %d\n", zero[N], one[N]);
 }
 
-int main(){
-	int idx;
-	int T;
+int main() {
+    int T;
+    scanf("%d", &T);
 
-	scanf("%d", &T);
+    for(int i = 0; i < T; ++i) {
+        int N;
+        scanf("%d", &N);
+        solve(N);
+    }
 
-	for(int i = 0; i < T; i++){
-		scanf("%d", &idx);
-		fibonacci(idx);
-	}
+    return 0;
 }
