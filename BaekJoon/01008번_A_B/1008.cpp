26cbe519765a6e5bc377ff166f3948aa2d10f345
bonomoon <moonbonoz@gmail.com>
2020-03-15 21:15:42 +0900

    solve: boj 1008

--- BaekJoon/01008번_A_B/1008.cpp
+++ BaekJoon/01008번_A_B/1008.cpp
@@ -1,22 +1,10 @@
 #include <cstdio>
-#include <cstring>
 
 int main() {
-	double a, b;
-	char c[14];
+    double a, b;
+    scanf("%lf %lf", &a, &b);
 
-	scanf("%lf %lf", &a, &b);
-	sprintf(c, "%.9lf", a/b);
+    printf("%.9f\n", a/b);
 
-	for (int i = strlen(c) - 1; i >= 0; i--) {
-		if (c[i] != '0') {
-			c[i+1] = '\0';
-			if (c[i] == '.') {
-				c[i] = '\0';
-			}
-			break;
-		}
-	}
-
-	printf("%s", c);
+    return 0;
 }
