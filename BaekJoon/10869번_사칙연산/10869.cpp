3cbab3c289ae3ab6e21e1b5faea5bc595df04db4
bonomoon <moonbonoz@gmail.com>
2020-03-19 20:13:12 +0900

    solved: 10869 사칙연산

--- BaekJoon/10869번_사칙연산/10869.cpp
+++ BaekJoon/10869번_사칙연산/10869.cpp
@@ -3,9 +3,6 @@
 int main() {
 	int a, b;
 	scanf("%d %d", &a, &b);
-	printf("%d\n", a+b);
-	printf("%d\n", a-b);
-	printf("%d\n", a*b);
-	printf("%d\n", a/b);
-	printf("%d\n", a%b);
+	printf("%d\n%d\n%d\n%d\n%d\n", a + b, a - b, a * b, a / b, a % b);
+	return 0;
 }
