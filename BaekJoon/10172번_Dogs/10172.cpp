1ecb8d9da364d7427edca2c69f4ccfdc0f7978b4
bonomoon <moonbonoz@gmail.com>
2020-03-19 19:56:37 +0900

    solved: 10172 Dogs

--- BaekJoon/10172번_Dogs/10172.cpp
+++ BaekJoon/10172번_Dogs/10172.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
+#include <cstdio>
 
-using namespace std;
-
-int main(){
-    cout << "|\\_/|" << endl;
-    cout << "|q p|   /}" << endl;
-    cout << "( 0 )\"\"\"\\" << endl;
-    cout << "|\"^\"`    |" << endl;
-    cout << "||_/=\\\\__|" << endl;
+int main() {
+    printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n");
+    return 0;
 }
