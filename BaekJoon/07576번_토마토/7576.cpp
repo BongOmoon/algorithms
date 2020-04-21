2055cba1bf6ded15a610a88084842bd3f28dcac3
bonomoon <moonbonoz@gmail.com>
2020-04-21 21:47:07 +0900

    solved: BOJ 7576

--- BaekJoon/07576번_토마토/7576.cpp
+++ BaekJoon/07576번_토마토/7576.cpp
@@ -1,89 +1,59 @@
 #include <cstdio>
 #include <queue>
 
-using namespace std;
+int box[1000][1000];
+std::queue<std::pair<int, int>> q;
 
-int box[1001][1001];
-int day;
-queue<int> qx;
-queue<int> qy;
+int checkTomato(int row, int col) {
+    int offset[] = {0, -1, 0, 1, 0}, day_cnt = -1;
 
-void checkTomato(int N, int M) {
-	while (!qx.empty()){
-		int size = qx.size();
+    while(!q.empty()) {
+        int q_size = q.size();
+        ++day_cnt;
 
-		for (int i = 0; i < size; i++) {
-			int x = qx.front(), y = qy.front();
-			qx.pop(), qy.pop();
-			if (box[y - 1][x] == 0 && y - 1 >= 1) {
-				box[y - 1][x] = 1;
-				qx.push(x);
-				qy.push(y - 1);
-			}
-			if (box[y + 1][x] == 0 && y + 1 <= N) {
-				box[y + 1][x] = 1;
-				qx.push(x);
-				qy.push(y + 1);
-			}
-			if (box[y][x - 1] == 0 && x - 1 >= 1) {
-				box[y][x - 1] = 1;
-				qx.push(x - 1);
-				qy.push(y);
-			}
-			if (box[y][x + 1] == 0 && x + 1 <= M) {
-				box[y][x + 1] = 1;
-				qx.push(x + 1);
-				qy.push(y);
-			}
-		}
-		day++;
-	}
-	day--;
+        for (int i = 0; i < q_size; ++i) {
+            int x = q.front().second, y = q.front().first;
+            q.pop();
+
+            for(int j = 0; j < 4; ++j) {
+                int offset_x = x + offset[j], offset_y = y + offset[j + 1];
+
+                if(offset_x >= 0 && offset_x < col && offset_y >= 0 && offset_y < row) {    // ì£¼ë³€ì— ì•ˆ ìµì€ í† ë§ˆí† ë¥¼ íì— ë„£ëŠ”ë‹¤.
+                    if(box[offset_y][offset_x] == 0) {
+                        box[offset_y][offset_x] = 1;
+                        q.push({offset_y, offset_x});
+                    }
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < row; ++i) {
+        for(int j = 0; j < col; ++j) {
+            if(box[i][j] == 0) {
+                return -1;
+            }
+        }
+    }
+
+    return day_cnt;
 }
 
 int main() {
-	int M, N;
-	scanf("%d %d", &M, &N);
+    int m, n;  // m : ìƒìžì˜ ê°€ë¡œ ì¹¸ì˜ ìˆ˜, n: ìƒìžì˜ ì„¸ë¡œ ì¹¸ì˜ ìˆ˜
+    scanf("%d %d", &m, &n);
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			int t;
-			scanf("%d", &t);
-			box[i][j] = t;
-		}
-	}
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < m; ++j) {
+            scanf("%d", &box[i][j]);
 
-	bool flag = true;
+            if(box[i][j] == 1) {      // 1ì¼ ë•Œ íì— ë„£ìŒ (0ì¼ ë•Œ ìµì€ í† ë§ˆí† ë“¤ì˜ ì´ˆê¸° ìƒíƒœ)
+                q.push({i, j});
+            }
+        }
+    }
 
-	//Åä¸¶Åä ³óÀåÀ» °Ë»çÇÑ´Ù. ¸¸¾à 1ÀÏ ¶§ Å¥¿¡ Áý¾î ³Ö´Â´Ù.
-	//flag°¡ ¹Ù²îÁö ¾ÊÀ¸¸é Åä¸¶Åä´Â ¸ðµÎ ÀÍ¾î ÀÖ´Â °ÍÀÌ´Ù.
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			if (box[i][j] == 1) {
-				qy.push(i);
-				qx.push(j);
-			}
-			else if (box[i][j] == 0)
-				flag = false;
-		}
-	}
+    printf("%d\n", checkTomato(n, m));
 
-	//Åä¸¶Åä°¡ ¸ðµÎ ÀÍ¾îÀÖÀ» ¶§
-	if (flag == true) {
-		printf("0");
-		return 0;
-	}
-
-	checkTomato(N, M);
-
-	//Åä¸¶Åä°¡ ¸ðµÎ ¾È ÀÍ¾úÀ» ¶§
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			if (box[i][j] == 0) {
-				printf("-1");
-				return 0;
-			}
-		}
-	}
-	printf("%d", day);
+    return 0;
 }
